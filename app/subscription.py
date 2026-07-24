1d8eb3a59f0f069757d0d0dda951f802fa0dd884
Чубака Чубака <chewbaccapro1337@gmail.com>
2026-07-24 15:32:58 +0300

    upgrade bot

--- app/subscription.py
+++ app/subscription.py
@@ -1,5 +1,3 @@
-from database import DB_NAME
-import sqlite3
 from datetime import datetime
 from database import get_connection
 
@@ -22,42 +20,13 @@ def check_subscription(user_id):
 
     conn.close()
 
-
     if not result:
         return False
 
-
-    expires = result[0]
-
-
     try:
-        expires_date = datetime.fromisoformat(expires)
+        expires_date = datetime.fromisoformat(result[0])
 
     except Exception:
         return False
 
-
-    if expires_date > datetime.now():
-        return True
-
-
-    return False
-
-def check_subscription(user_id):
-
-    conn = sqlite3.connect(DB_NAME)
-    cur = conn.cursor()
-
-    cur.execute(
-        "SELECT expires_at FROM subscriptions WHERE user_id=?",
-        (user_id,)
-    )
-
-    row = cur.fetchone()
-
-    conn.close()
-
-    if not row:
-        return False
-
-    return datetime.fromisoformat(row[0]) > datetime.now()
+    return expires_date > datetime.now()
