1834b0ccb2994789ec0319396fc684898e87621c
Чубака Чубака <chewbaccapro1337@gmail.com>
2026-07-26 22:10:38 +0300

    update_bot

--- app/market_context.py
+++ app/market_context.py
@@ -1,15 +1,49 @@
+import yfinance as yf
+
+
+def get_price(ticker):
+
+    try:
+
+        data = yf.Ticker(ticker)
+
+        price = data.history(
+            period="1d",
+            interval="5m"
+        )
+
+        if price.empty:
+            return None
+
+        last = price["Close"].iloc[-1]
+
+        return round(float(last), 2)
+
+
+    except Exception as e:
+
+        print("MARKET DATA ERROR:", ticker, e)
+
+        return None
+
+
+
 def get_market_context():
 
     return {
 
-        "BTC": None,
+        "DXY": {
+            "price": get_price("DX-Y.NYB")
+        },
 
-        "DXY": None,
 
-        "GOLD": None,
+        "GOLD": {
+            "price": get_price("GC=F")
+        },
 
-        "SP500": None,
 
-        "BTC_DOMINANCE": None
+        "SP500": {
+            "price": get_price("^GSPC")
+        }
 
     }
