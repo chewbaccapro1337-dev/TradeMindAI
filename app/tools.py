7046d4afacc2992779c3605cf4d4bcfdd4be6a53
Чубака Чубака <chewbaccapro1337@gmail.com>
2026-07-26 16:55:29 +0300

    update_bot

--- app/tools.py
+++ app/tools.py
@@ -1,23 +1,24 @@
-from analysis import make_report
-from journal import (
-    build_statistics_text,
-    build_last_trades_text
-)
-
-
 def btc_analysis_tool(user_id):
+
+    from analysis import make_report
 
     return make_report()
 
 
+
 def statistics_tool(user_id):
+
+    from journal import build_statistics_text
 
     return build_statistics_text(
         user_id
     )
 
 
+
 def last_trades_tool(user_id):
+
+    from journal import build_last_trades_text
 
     return build_last_trades_text(
         user_id
